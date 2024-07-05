1a433af9f48ca395ffad6b97ca8f6656daf15388
Manassarn Manoonchai <manassarn@gmail.com>
2024-07-06 00:19:40 +0700

    Add description

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @memes = [
-      OpenStruct.new(title: 'Tresbien', url: "#", image_url: '/tresbien.png', description: '', wip: true),
-      OpenStruct.new(title: 'Bkk Sticker', url: "https://bkk-sticker.vercel.app", image_url: '/bkk-sticker.jpg', description: '', wip: false),
+      OpenStruct.new(title: 'Tresbien', url: "#", image_url: '/tresbien.png', description: 'ป้ายทะเบียนรถ', wip: true),
+      OpenStruct.new(title: 'Bkk Sticker', url: "https://bkk-sticker.vercel.app", image_url: '/bkk-sticker.jpg', description: 'สติ๊กเกอร์รถไฟฟ้า', wip: false),
     ]
   end
 end
